3c261e19dd18b862a1ef9969f15735344f4324e1
JulioAmestica <57876587+JulioAmestica@users.noreply.github.com>
2022-03-28 01:56:34 -0300

    Agregar archivos de proyecto.

---
+++ ChatNet/ChatNetHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatNet
+{
+    public class ChatNetHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
